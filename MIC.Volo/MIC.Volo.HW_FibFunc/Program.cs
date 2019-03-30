52714816111f42cbebaea43a60fcd07ed498f5d0
mic.armenia <mic.armenia@hotmail.com>
2019-03-30 14:13:44 +0400

    Add new project

---
+++ MIC.Volo/MIC.Volo.HW_FibFunc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIC.Volo.HW_FibFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
