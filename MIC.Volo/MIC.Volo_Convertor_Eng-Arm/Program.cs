809141e6e384f0b8da27ae97404ea15b5b778671
mic.armenia <mic.armenia@hotmail.com>
2019-04-06 19:09:35 +0400

    Add project

---
+++ MIC.Volo/MIC.Volo_Convertor_Eng-Arm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIC.Volo_Convertor_Eng_Arm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
