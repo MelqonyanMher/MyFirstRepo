cda6a610d184be15526efbb283538227f80b9781
mic.armenia <mic.armenia@hotmail.com>
2019-04-09 14:28:29 +0400

    Add class MyWebClient

---
+++ MIC.Volo/MIC.Volo_HTMLtoFile/MyWebClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace MIC.Volo_HTMLtoFile
+{
+    class MyWebClient
+    {
+
+       
+
+    }
+}
---
+++ MIC.Volo/MIC.Volo_HTMLtoFile/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MIC.Volo_HTMLtoFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyWebClient my = new MyWebClient("Http://facebook.com");
+           my.DownloadInFile(@"C:\Users\Melqonyan\Desktop\my.txt");
+        }
+    }
+}
