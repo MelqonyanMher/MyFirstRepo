a2a62bd88b6da544de375625d04b18e188306efb
mic.armenia <mic.armenia@hotmail.com>
2019-04-09 14:31:42 +0400

    Add Func DownloadInFile

--- MIC.Volo/MIC.Volo_HTMLtoFile/MyWebClient.cs
+++ MIC.Volo/MIC.Volo_HTMLtoFile/MyWebClient.cs
@@ -25,6 +25,33 @@ namespace MIC.Volo_HTMLtoFile
             return web.DownloadString(uri);
         }
 
+        public void DownloadInFile(string adress)
+        {
+            string s = this.Download();
+            StreamWriter writer = new StreamWriter(adress, true, System.Text.Encoding.Default);
+            Console.WriteLine(" ||||||||||||||||||||");
+            int j = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                if (i < s.Length / 20 * j)
+                {
+                    writer.Write(s[i]);
+                }
+                else
+                {
+                    writer.Write(s[i]);
+                    Console.SetCursorPosition(j, 0);
+                    Console.Write("|");
+                    Console.SetCursorPosition(10, 1);
+                    Console.WriteLine($"{(i * 100 / s.Length + 1)}%");
+                    j++;
+                }
 
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+            writer.Dispose();
+        }
     }
 }
