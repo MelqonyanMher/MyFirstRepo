7e6ab004a69d6902830c7048c6bb5908491f2050
MherMelkonyan <melqonyan.mher@mail.ru>
2019-04-25 19:16:59 +0400

    Add method FillBase

--- MIC.Volo/MIC.Volo.DBGenerator/Program.cs
+++ MIC.Volo/MIC.Volo.DBGenerator/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
 
 namespace MIC.Volo.DBGenerator
 {
@@ -10,6 +10,34 @@ namespace MIC.Volo.DBGenerator
     {
         static void Main(string[] args)
         {
+            FillBase();
+        }
+        static void FillBase()
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLlocaldb;Initial Catalog=PFM;Integrated Security=True";
+            SqlConnection conection = new SqlConnection(connectionString);
+            SqlCommand comand = new SqlCommand();
+            conection.Open();
+            string readId = "Select Id from dbo.Category";
+            comand.CommandText = readId;
+            comand.Connection = conection;
+            SqlDataReader reader = comand.ExecuteReader();
+            List<string> categoryId = new List<string>();
+            while (reader.Read())
+            {
+                categoryId.Add(reader[0].ToString());
+            }
+            reader.Close();
+            string instComand;
+            Random rd = new Random();
+            for (int i = 0; i < 1000000; i++)
+            {
+                instComand = $"insert into Wallet (CategoryId,Amount,Day) values ('{categoryId[rd.Next(categoryId.Count)]}',{rd.Next(-200000, 600000)},getutcdate())";
+                comand.CommandText = instComand;
+                comand.ExecuteNonQuery();
+            }
+            comand.Dispose();
+            conection.Dispose();
         }
     }
 }
