c18c8bd92e2b34743e25d63129f660bbcfbed8a5
MherMelkonyan <melqonyan.mher@mail.ru>
2019-04-30 21:07:44 +0400

    Add efcore

---
+++ MIC.Volo/MIC.Volo.PFM.EFCore/PFMDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MIC.Volo.PFM.EFCore
+{
+    class PFMDbContext:DbContext
+    {
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+    }
+
+    public class Wallet
+    {
+        public int WalletId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Day { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Comment { get; set; }
+
+        public int CategorytId { get; set; }
+        public Category Category;
+    }
+
+    public class Category
+    {
+        public int CategorytId { get; set; }
+        public String Title { get; set; }
+
+        public List<Wallet> wallets;
+    }
+}
---
+++ MIC.Volo/MIC.Volo.PFM.EFCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MIC.Volo.PFM.EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
